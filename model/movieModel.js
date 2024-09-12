b089bb7c0161fd007230d56362bf9e38adcf52bf
John Doe <johndoe@email.com>
2024-09-12 01:30:16 +0100

    model is created

--- model/movieModel.js
+++ model/movieModel.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-const movieSchema = new mongoose,Schema({
+const movieSchema = new mongoose.Schema({
     movieposter:{type:String,required:true},
     moviename:{type:String,required:true},
     rating:{type:String,required:true},
--- model/userModel.js
+++ model/userModel.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
 
-const userSchema = new mongoose,Schema({
+const userSchema = new mongoose.Schema({
     name:{type:String,required:true},
     age:{typr:Number,required:true},
     phone_number:{type:Number,required:true},
