1deba6f2d2091e8aabd883c5ee74f9c3f7704ce1
John Doe <johndoe@email.com>
2024-09-12 10:12:26 +0100

    updated models

--- model/orderModel.js
+++ model/orderModel.js
@@ -2,8 +2,8 @@ const mongoose = require ('mongoose')
 
 const orderSchema = new mongoose.Schema({
 movieName:{type:String,required:true},
-Rating:{type:Number,required:true},
-Amount:{type:Number,required:true},
+rating:{type:Number,required:true},
+amount:{type:Number,required:true},
 })
 
 const Order = mongoose.model("Order",orderSchema)
