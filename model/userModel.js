937789bfe30c4ac9e7c3f37396a2ac924c8f8d23
Fumika Mikami <fumicha.3fan1@gmail.com>
2025-05-09 12:11:21 +0900

    up

--- model/userModel.js
+++ model/userModel.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken")
 const userSchema = new mongoose.Schema({
     name:{type:String,required:true},
     lastname:{type:String,required:true},
-    age:{type:Number,required:true},
+    // age:{type:Number,required:true},
     gender:{type:String,required:true},
     phone_number:{type:String,required:true},
     email:{type:String,required:true},
