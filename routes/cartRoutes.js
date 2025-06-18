4bcddea3cb8edab11aefaf082bdb2c01c5973dcf
Fumika Mikami <fumicha.3fan1@gmail.com>
2025-06-18 13:39:17 +0900

    cartroute

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -30,28 +30,29 @@ const {conditionalAuth} = require('../middleware/auth')
 // })
 
 
-router.post('/addcart',conditionalAuth, async (req, res) => {
-  try {
-    const cartData = new Cart({ ...req.body }); // Removed 'owner' field since auth isn't needed
-
-    if (cartData) {
-      await cartData.save();
-      res.status(200).send({ cartData, message: "Cart has been added successfully!" });
-      console.log(cartData, "Cart has been added successfully!");
-    } else {
-      res.send({ message: "This movie is already added" });
+router.post('/addcart',auth,async(req,res)=>{
+     try{
+        const cartData = new Cart({
+            ...req.body,
+            owner:req.user._id
+        })
+        if(cartData){
+            await cartData.save()
+        res.status(200).send({cartData:cartData,message:"Cart item has been added successfully!"})
+        console.log(cartData,"Cart Item has been added successfully!")
+    }else{
+          return  res.send({message:"This movie is already added"})
+        }    
+    }catch(e){
+        // res.status(500).send({message:"Some internal error"})
+        console.log("error",e.code) // e >> entire messages show, e.code >> only error code show, // "e" is object
+        if (e.code==11000){
+           return  res.send({message:"This is a duplicated key"})
+        }else{
+            return res.send({message:"some internal error"})
+        }
     }
-  } catch (e) {
-    console.error("Error:", e.code); // Logs error code if available
-
-    if (e.code === 11000) {
-      res.send({ message: "This is a duplicated key" });
-    } else {
-      res.send({ message: "Some internal error occurred" });
-    }
-  }
-});
-
+})
 
 // GetCart - 
 router.get('/cart',conditionalAuth,async(req,res)=>{
@@ -88,5 +89,20 @@ router.delete('/clearcart',auth,async(req,res)=>{
 // res.send({message:"some internal error"})
 // }
 })
+
+// DELETE 
+// router.delete('/delete-cart-item/:id',auth,async(req,res)=>{
+//     try{
+//     console.log("Delete Cart Item by ID",req.params.id)
+//     const deleteWish = await Cart.findOneAndDelete({
+//         _id:req.params.id,owner:req.user._id
+//     })
+//     if(!deleteWish){
+//         res.send({message:"Cart Items Not Found"})
+//     }res.send({message:"Cart Item has been deleted successfully",deleteWish})
+//  }catch(e){
+//             res.send({message:"Some Internal Error"})
+//         }
+// })
 
 module.exports=router
